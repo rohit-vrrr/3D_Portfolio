91d93367425cd5e4264134e076d8d3e3a3de6697
Rohit V <rohit.v@necsws.com>
2026-02-20 11:55:30 +0530

    removed auto rotation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,28 +1,14 @@
-import React, { Suspense, useEffect, useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-  const meshRef = useRef();
-
-  const [isHovered, setIsHovered] = useState(false);
-
-  useFrame((state, delta) => {
-    if (!isHovered && meshRef.current) {
-      meshRef.current.rotation.y += delta * 0.1;
-      // 0.1 = smooth elegant rotation
-    }
-  });
 
   return (
-    <mesh
-      ref={meshRef}
-      onPointerOver={() => setIsHovered(true)}
-      onPointerOut={() => setIsHovered(false)}
-    >
+    <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
@@ -69,7 +55,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="always"
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
